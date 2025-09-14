5b7b7b97b41c714b23d896257c72ec3406cfda87
jspaterson000 <jacob.paterson000@gmail.com>
2025-09-14 15:30:06 +1000

    Fix JSX fragment and div structure error

--- src/pages/StartAssessment.jsx
+++ src/pages/StartAssessment.jsx
@@ -427,7 +427,6 @@ export default function StartAssessment() {
                       </div>
                     )}
                     {renderStep()}
-                  </>
                 </div>
                 
                 {/* Side Panel with Tools */}
